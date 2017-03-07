598b15422b4bda0fd134221ba865052d8002bc77
Jan Gosmann <jgosmann@uwaterloo.ca>
2017-03-07 13:43:42 -0500

    fixup! Add basic spike raster plotting function.

--- nengo_extras/tests/test_spikeplot.py
+++ nengo_extras/tests/test_spikeplot.py
@@ -8,7 +8,7 @@ from nengo_extras.spikeplot import plot_spikes
 
 @pytest.mark.noassertions
 def test_plot_spikes(plt, seed, RefSimulator):
-    with nengo.Network(seed) as model:
+    with nengo.Network(seed=seed) as model:
         ens = nengo.Ensemble(10, 1)
         inp = nengo.Node(np.sin)
         nengo.Connection(inp, ens)
